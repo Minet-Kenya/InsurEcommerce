c4f0cc44f404669ed3c17f30c6015ddaa0436873
WaksKevin <wakskevin@outlook.com>
2024-01-29 13:10:14 +0300

    + individual_solutions & corporate_products pages

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("individual_solutions/", views.individual_solutions, name="individual_solutions"),
+    path("corporate_product/", views.corporate_product, name="corporate_product"),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, "ecommerce/home.html", context)
+
+
+def individual_solutions(request):
+    return render(request, "ecommerce/individual_solutions.html", {})
+
+
+def corporate_product(request):
+    return render(request, "ecommerce/corporate_product.html", {})
