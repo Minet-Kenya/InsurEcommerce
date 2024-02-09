4d8fe3c20a2c449f2b193b4c9783674961e725ac
WaksKevin <wakskevin@outlook.com>
2024-02-09 10:41:41 +0300

    sass watch command for tracking changes to custom bootstrap

---
+++ base/management/commands/sasswatch.py
@@ -0,0 +1,29 @@
+
+from django.core.management.base import BaseCommand
+import subprocess
+import sys
+
+class Command(BaseCommand):
+    help = "Watch SCSS files and compile them using sass"
+    def handle(self, *args, **options):
+        scss_input_path = "./base/static/base/scss/custom.scss"
+        css_output_path = "./base/static/vendor/bootstrap/bootstrap.custom.min.css"
+        # Run the sass command
+        sass_process = subprocess.Popen(
+            [
+                "sass",
+                "--watch",
+                scss_input_path,
+                css_output_path,
+                "--style",
+                "compressed",
+            ]
+        )
+        try:
+            # Wait for the sass process to finish (Ctrl-C will raise KeyboardInterrupt)
+            sass_process.wait()
+        except KeyboardInterrupt:
+            self.stdout.write(self.style.SUCCESS("Sass compilation stopped manually."))
+            # Terminate the sass process
+            sass_process.terminate()
+            sys.exit(0)
