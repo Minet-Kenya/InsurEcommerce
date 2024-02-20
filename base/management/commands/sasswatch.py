6b583678f503099b7495687d2bd33cecb9ec09f5
WaksKevin <wakskevin@outlook.com>
2024-02-20 11:37:55 +0300

    updated sasswatch management command

--- base/management/commands/sasswatch.py
+++ base/management/commands/sasswatch.py
@@ -1,29 +1,18 @@
-
 from django.core.management.base import BaseCommand
-import subprocess
-import sys
+import os
 
 class Command(BaseCommand):
     help = "Watch SCSS files and compile them using sass"
+
     def handle(self, *args, **options):
         scss_input_path = "./base/static/base/scss/custom.scss"
         css_output_path = "./base/static/base/css/bootstrap.custom.min.css"
-        # Run the sass command
-        sass_process = subprocess.Popen(
-            [
-                "sass",
-                "--watch",
-                scss_input_path,
-                css_output_path,
-                "--style",
-                "compressed",
-            ]
-        )
+
+        # Define the command to compile SCSS using node-sass
+        compile_command = f"sass --watch {scss_input_path} {css_output_path} --style compressed"
+
         try:
-            # Wait for the sass process to finish (Ctrl-C will raise KeyboardInterrupt)
-            sass_process.wait()
+            # Execute the compile command
+            os.system(compile_command)
         except KeyboardInterrupt:
             self.stdout.write(self.style.SUCCESS("Sass compilation stopped manually."))
-            # Terminate the sass process
-            sass_process.terminate()
-            sys.exit(0)
