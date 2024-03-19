fd638f1337c02c869e1ffaad5e05fc732eaf0e4d
wakskevin <kevin.wakhisi@loxinformatics.com>
2024-03-19 14:31:31 +0300

    updates to Home

--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -1,30 +1,30 @@
 import { Link } from 'react-router-dom'
 
 import './Header.css'
-import logoFull from '../../assets/images/logo-full.png'
+// import logoFull from '../../assets/images/logo-full.png'
 import logoFullWhite from '../../assets/images/logo-full-white.png'
-import logoSmall from '../../assets/images/logo-small.png'
+// import logoSmall from '../../assets/images/logo-small.png'
 import logoSmallWhite from '../../assets/images/logo-small-white.png'
 
 
 export function Header() {
 
-    const logo = document.body.id !== 'index' ? logoFull : logoFullWhite;
-    const logo_small = document.body.id !== 'index' ? logoSmall : logoSmallWhite;
+    // const logo = document.body.id !== 'index' ? logoFull : logoFullWhite;
+    // const logo_small = document.body.id !== 'index' ? logoSmall : logoSmallWhite;
 
     return (
         <header id="header" className="fixed-top w-100 pt-2 px-4">
             <div className="container-fluid d-flex align-items-center justify-content-between py-2">
 
                 <Link to="/" className="logo m-0 p-0 d-none d-lg-block">
-                    <img src={logo}
+                    <img src={logoFullWhite}
                         width="250px"
                         height=""
                         alt="logo"
                         className="inner-pages img-fluid" />
                 </Link>
                 <Link to="/" className="logo m-0 p-0 d-block d-lg-none">
-                    <img src={logo_small}
+                    <img src={logoSmallWhite}
                         width="70px"
                         height=""
                         alt="logo"
@@ -39,29 +39,29 @@ export function Header() {
                             <Link className="nav-link" to="/contact"><span className="bi bi-telephone-fill">&nbsp;&nbsp;Contact</span></Link>
                         </li>
                         <li>
-                            <Link className="nav-link" to="/login">
+                            <Link className="nav-link" to="/">
                                 <span className="bi bi-person-check-fill">&nbsp;&nbsp;Login</span>
                             </Link>
                         </li>
                         <li>
-                            <Link className="nav-link" to="/register">
+                            <Link className="nav-link" to="/">
                                 <span className="bi bi-person-plus-fill">&nbsp;&nbsp;Sign Up</span>
                             </Link>
                         </li>
                         <li>
-                            <Link className="nav-link" to="/profile">
+                            <Link className="nav-link" to="/">
                                 <span className="bi bi-person-fill">&nbsp;&nbsp;Profile</span>
                             </Link>
                         </li>
                         <li>
-                            <Link className="nav-link" to="/logout">
+                            <Link className="nav-link" to="/">
                                 <span className="bi bi-box-arrow-right">&nbsp;&nbsp;Logout</span>
                             </Link>
                         </li>
                     </ul>
                     <i className="bi bi-list mobile-nav-toggle"></i>
                 </nav>
-                <Link className="btn-getstarted text-decoration-none" to="/retail">
+                <Link className="btn-getstarted text-decoration-none" to="/">
                     <h2 className="fs-6 m-0">Ecommerce</h2>
                 </Link>
             </div>
--- web/src/index.js
+++ web/src/index.js
@@ -3,22 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AOS from 'aos';
-import GLightbox from 'glightbox';
-import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
 
 // CSS/Styles
 import 'aos/dist/aos.css';
-import 'glightbox/dist/css/glightbox.min.css';
-import 'swiper/css';
-import 'swiper/css/pagination';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.scss'
 
-// Local Views
-import {Landing} from './views/Landing/Landing';
-import {Contact} from './views/Contact/Contact';
-import {Auth} from './views/Auth/Auth';
+// Local views
+import { Landing, Contact, Auth } from './views/Home/Home';
 
 // Initialize AOS
 AOS.init({
@@ -26,35 +18,15 @@ AOS.init({
   easing: "ease-in-out"
 });
 
-// Initialize GLightbox
-const glightbox = GLightbox({
-  selector: '.glightbox'
-});
-
-// Initialize Swiper
-const swiper = new Swiper('.swiper', {
-
-  modules: [Pagination],
-  loop: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  speed: 700,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: true,
-  },
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Landing />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/auth" element={<Auth />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
