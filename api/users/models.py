b9923f9a0db22beb6fc18c8209225db17b62633b
samuel267 <samthewebdev@gmail.com>
2024-06-29 09:00:31 +0300

    pulling from main

---
+++ api/users/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils import timezone
+import uuid
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(username, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        CLIENT = "CLIENT", "Client"
+
+    base_role = "SUPERUSER"
+
+    username_validator = UnicodeUsernameValidator()
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    role = models.CharField(max_length=50, choices=Role.choices, blank=True)
+    image = models.ImageField(
+        upload_to="students/",
+        help_text="Upload an image that is 600x600",
+        blank=True,
+        default="defaults/default_profile.jpg",
+    )
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text=(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": ("A user with that number already exists."),
+        },
+    )
+
+    email = models.EmailField(max_length=255, unique=True)
+    krapin = models.CharField(max_length=255)
+    idNumber = models.CharField(max_length=255)
+    dob = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30,default=False)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+    personal_info = models.JSONField(blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)  # Automatically set to current date when object is created
+    last_modified = models.DateField(auto_now=True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
+
+    objects = UserManager()
+
+    def save(self, *args, **kwargs):
+        self.role = self.base_role
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        # return f"{self.first_name} {self.last_name}"
+        return self.email
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class ClientManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        return queryset.filter(role=User.Role.CLIENT)
+
+
+class Client(User):
+    class Meta:
+        proxy = True
+
+    base_role = User.Role.CLIENT
+    clients = ClientManager()
